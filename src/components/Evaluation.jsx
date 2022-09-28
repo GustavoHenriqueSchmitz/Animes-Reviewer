93f96409b5cb4223e85857f9917391fb7fbbf428
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2022-09-28 12:29:04 -0300

    design

--- src/components/Evaluation.jsx
+++ src/components/Evaluation.jsx
@@ -2,11 +2,15 @@ import style from './css/Evaluation.module.css'
 
 function AnimeEvaluation() {
     return (
-    <div id={style.evaluation}>
-        <h1 id={style.note}>7.5</h1>
-        <label>Insira sua avaliação</label>
-        <input id={style.evaluationInput} type="number" min="0" max="10" step="0.25"  />
-    </div>
+    <>
+        <div id={style.note}>
+            <h1>7.5</h1>
+        </div>
+        <div id={style.evaluation}>
+            <label>Insira sua avaliação</label>
+            <input type="number" min="0" max="10" step="0.25"/>
+        </div>
+    </>
     )
 }
 
