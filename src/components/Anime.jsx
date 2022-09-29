9c68a0c3b9eb9af96801025dfbaf84888236a4ac
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2022-09-29 11:33:56 -0300

    creating-design

--- src/components/Anime.jsx
+++ src/components/Anime.jsx
@@ -3,7 +3,7 @@ import style from './css/Anime.module.css'
 function Anime() {
     return (
         <>
-            <h1 id={style.animeName}>Nome do anime</h1>
+            <h1 className={style.titles} id={style.animeName}>Nome do anime</h1>
             <img id={style.animeImage} src="https://media.kitsu.io/anime/poster_images/1/original.jpg" alt="Imagem ilustrando o anime ou manga."/>
             <button id={style.animeButton}><p className={style.texts}>Próximo Anime</p></button>
         </>
--- src/components/Evaluation.jsx
+++ src/components/Evaluation.jsx
@@ -3,14 +3,19 @@ import style from './css/Evaluation.module.css'
 function AnimeEvaluation() {
     return (
     <>
-        <div id={style.note}>
-            <p>7.5</p>
+        <div id={style.evaluationTitle}>
+            <h1 className={style.titles}>Avaliação</h1>
         </div>
         <div id={style.evaluation}>
-            <form action="">
-                <label>Insira sua avaliação</label>
-                <input type="number" min="0" max="10" step="0.25"/>
-            </form>
+            <div id={style.evaluationNote}>
+                <p>7.5</p>
+            </div>
+            <div id={style.evaluationForm}>
+                <form action="">
+                    <label className={style.texts}>Insira sua avaliação</label>
+                    <input type="number" min="0" max="10" step="0.25"/>
+                </form>
+            </div>
         </div>
     </>
     )
