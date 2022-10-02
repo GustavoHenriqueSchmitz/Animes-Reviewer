214a21a273f4eba574b5b9e58f34836e8c13eaf3
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2022-10-01 22:27:34 -0300

    vertical-responsivity

---
+++ src/components/js/render.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+function GetWindowSize() {
+// useStates variables
+  const [isDesktop, setDesktop] = useState(window.innerWidth > 1000)
+
+  // Check the window size for the rendering
+  const updateMedia = () => {
+    setDesktop(window.innerWidth > 1000);
+  }
+
+  useEffect(() => {
+    window.addEventListener("resize", updateMedia);
+    return () => window.removeEventListener("resize", updateMedia);
+  })
+
+  return isDesktop
+}
+
+export { GetWindowSize }
