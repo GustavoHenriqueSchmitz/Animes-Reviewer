a1ee63f90f9704400b40a93ac3082f321c8eaa77
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2022-10-02 11:25:05 -0300

    vrtical-responsivity

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('base-div'));
+const root = ReactDOM.createRoot(document.getElementById('baseDiv'));
 root.render(
   <React.StrictMode>
     <App />
