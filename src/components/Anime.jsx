72dec7f722cba685e7406e0219a769a8ef0a2a2d
GustavoHenriqueSchmitz <gustavohenriqueschmitz568@gmail.com>
2022-10-04 14:34:24 -0300

    getting-image-and-name

--- src/components/Anime.jsx
+++ src/components/Anime.jsx
@@ -1,23 +1,22 @@
 import style from './css/Anime.module.css'
-import { useEffect, useState } from 'react'
+import { useReducer } from 'react'
 import { RenderizeAnimeImage } from './js/api'
-import { GetWindowSize } from './js/render'
+import { name, image } from './js/api'
 
 function Anime() {
 
-    const [image, setImage] = useState('https://media.kitsu.io/anime/poster_images/1/original.jpg')
+    const [ignored, forceUpdate] = useReducer(x => x + 1, 0)
 
-    useEffect(async ()=>{
-       const renderizedImage =  await RenderizeAnimeImage()
-       setImage(renderizedImage)
-       console.log(image)
-    },[])
+    async function renderInformations() {
+        await RenderizeAnimeImage()
+        forceUpdate()
+    }
 
     return (
         <>
-            <h1 className={style.titles} id={style.animeName}>Nome do anime</h1>
-            {image && <img id={style.animeImage} src={image} alt="Imagem ilustrando o anime ou manga."/> }
-            <button id={style.animeButton}><p className={style.texts}>Próximo Anime</p></button>
+            <h1 className={style.titles} id={style.animeName}>{name}</h1>
+            <img id={style.animeImage} src={image} alt="Imagem ilustrando o anime ou manga."/>
+            <button id={style.animeButton} onClick={renderInformations}><p className={style.texts}>Próximo Anime</p></button>
         </>
     )
 }
--- src/components/js/api.js
+++ src/components/js/api.js
@@ -1,14 +1,26 @@
+import { useState } from "react"
+
+let name = 'Cowboy Bebop'
+let image = 'https://media.kitsu.io/anime/poster_images/1/original.jpg'
+let description = ''
+
 async function RenderizeAnimeImage() {
 
-    const animeId = Math.floor(Math.random() * (5000 - 1) + 1)
-    const url = `https://kitsu.io/api/edge/anime/1`
-
-    // GET request using fetch with error handling
-    let data = await fetch(url)
-    data = await data.json();
-    data = data.data.attributes.posterImage.original
+    while (true) {
+        const animeId = Math.floor(Math.random() * (5000 - 1) + 1)
+        const url = `https://kitsu.io/api/edge/anime/${animeId}`
     
-    return data
+        // GET request using fetch with error handling
+        const response = await fetch(url)
+
+        if (response.status === 200) {
+            const data = await response.json()
+            name = data.data.attributes.canonicalTitle
+            image = data.data.attributes.posterImage.original
+            break
+        }
+        continue
+    }
 }
 
-export { RenderizeAnimeImage }
+export { RenderizeAnimeImage, name, image, description}
